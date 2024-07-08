582621cc8e20b5fdfbc227937d6b1f965c8e1f27
Guilherme Leobas <guilhermeleobas@gmail.com>
2024-07-08 13:27:01 +0000

    Let dynamo inline functional_call
    ghstack-source-id: e0b523629de6341bff1a17291dc8c1a3149626f0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128646

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -1192,6 +1192,7 @@ class FunctorchHigherOrderVariable(UserFunctionVariable):
                 "jacfwd": "jacfwd",
                 "hessian": "hessian",
                 "linearize": "linearize",
+                "functional_call": "functional_call",
             }.get(name)
             assert name is not None
             unimplemented(
